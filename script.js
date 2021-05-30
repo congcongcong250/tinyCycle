b71e3a8087d98b4e258024c26ec9bd74857e4746
Lee Wang <lwang5@atlassian.com>
2021-05-30 20:21:13 +1000

    Make dynamic run() for input drivers

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function main(sources) {
             .flatten()
             .map(i => `Seconds elapsed: ${i}`),
         log: xs.periodic(2000)
-            .fold(prev => prev + 1, 0)
     }
 }
 
@@ -50,14 +49,26 @@ function run(mainFn, drivers) {
     // b = f(fakeA)
     // a = g(b)
     // fakeA.behaveLike(a);
-    const fakeDOMSink = xs.create();
-    const domSource = domDriver(fakeDOMSink);
-    const sink = mainFn({ DOM: domSource });
-    fakeDOMSink.imitate(sink.DOM);
+    // const fakeDOMSink = xs.create();
+    // const domSource = domDriver(fakeDOMSink);
+    // const sink = mainFn({ DOM: domSource });
+    // fakeDOMSink.imitate(sink.DOM);
 
-    // Object.keys(sink).forEach(k => {
-    //     drivers[k](sink[k]);
-    // })
+    const fakeDOMSinks = {};
+    Object.keys(drivers).forEach(k => {
+        fakeDOMSinks[k] = xs.create();
+    })
+
+    const sources = {};
+    Object.keys(drivers).forEach(k => {
+        sources[k] = drivers[k](fakeDOMSinks[k]);
+    })
+
+    const sinks = mainFn(sources);
+
+    Object.keys(fakeDOMSinks).forEach(k => {
+        fakeDOMSinks[k].imitate(sinks[k]);
+    })
 }
 
 run(main, {

b64cb91e8abbfc009c5c479f485d68a8f75a10f3
Lee Wang <lwang5@atlassian.com>
2021-05-30 16:06:03 +1000

    Separate log logic and dom logic

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Logic
 function main(){
-    return xs.periodic(1000)
-        .fold(prev => prev + 1, 0)
-        .map(i => `Seconds elapsed: ${i}`);
+    return {
+        DOM: xs.periodic(1000)
+            .fold(prev => prev + 1, 0)
+            .map(i => `Seconds elapsed: ${i}`),
+        log: xs.periodic(2000)
+            .fold(prev => prev + 1, 0)
+    }
 }
 
 // Effects
@@ -25,5 +29,5 @@ function logDriver(msg$){
 
 // A one-way flow sink
 const sink = main();
-domDriver(sink);
-logDriver(sink);
+domDriver(sink.DOM);
+logDriver(sink.log);

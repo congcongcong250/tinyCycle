a7c03776f772b5a01293bab68c2b6fb67cc254ae
Lee Wang <lwang5@atlassian.com>
2021-05-30 16:01:06 +1000

    Add Driver: - refactor logic into main function - refactor effects into driver function - with reactive subscriber pattern

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 // Logic
-xs.periodic(1000)
-    .fold(prev => prev + 1, 0)
-    .map(i => `Seconds elapsed: ${i}`)
+function main(){
+    return xs.periodic(1000)
+        .fold(prev => prev + 1, 0)
+        .map(i => `Seconds elapsed: ${i}`);
+}
+
 // Effects
-    .subscribe({
+function domDriver(text$){   
+    text$.subscribe({
         next: str => {
             const el = document.querySelector('#app');
             el.textContent = str;
         }
     })
+}
+
+function logDriver(msg$){
+    msg$.subscribe({
+        next: msg => {
+            console.log(msg);
+        }
+    });
+}
+
+// A one-way flow sink
+const sink = main();
+domDriver(sink);
+logDriver(sink);
